e70edcf90d81535f591dd479abeea30c4c9ae2c7
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-06-04 17:01:12 +0700

    feat: integrated login controller with login inteceptor

--- src/main/java/com/example/javaee/controller/GoogleLoginController.java
+++ src/main/java/com/example/javaee/controller/GoogleLoginController.java
@@ -2,15 +2,8 @@ package com.example.javaee.controller;
 
 import com.example.javaee.beans.AppConfigGoogleAccount;
 import com.example.javaee.beans.SignInGoogleAccount;
-import com.example.javaee.dto.AccessTokenResponse;
-import com.example.javaee.dto.OpenIdClaims;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Form;
-import org.apache.http.client.fluent.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,73 +54,14 @@ public class GoogleLoginController {
     public String handleRedirectRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String code = request.getParameter("code");
         String receivedState = request.getParameter("state");
-        String scope = request.getParameter("scope");
         System.out.println("code:" + code);
         System.out.println("receivedState:" + receivedState);
         System.out.println("currentState :" + this.state);
         if (!receivedState.equals(this.state)) {
             System.out.println("Invalid state! Authorization session failed!");
-            return "index";
+            return "redirect:/index";
         }
-        AccessTokenResponse accessTokenResponse = getToken(code);
-//        System.out.println("accessToken:" + accessTokenResponse);
-        OpenIdClaims user = getUserInfo(accessTokenResponse.getAccessToken());
-        if (!user.getEmail().equals(this.signInGoogleAccount.getEmail())) {
-            System.out.println("Forbidden Google account!");
-        }
-        else {
-            System.out.println("Authorized!");
-        }
-//        System.out.println(user);
-        return "redirect:/index.htm";
+        return "redirect:/admin/index.htm?code=" + code;
     }
 
-    /**
-     * This method use the code from the redirect request of Google to request the
-     * access token from Google for fetching User's Information
-     *
-     * @param code
-     * @return
-     * @throws ClientProtocolException
-     * @throws IOException
-     */
-    private AccessTokenResponse getToken(String code) throws ClientProtocolException, IOException {
-        final String API_END_POINT = "https://oauth2.googleapis.com/token";
-        final String GRAND_TYPE = "authorization_code";
-
-        String response = Request.Post(API_END_POINT)
-                .bodyForm(Form.form()
-                        .add("client_id", this.appConfigGoogleAccount.getClientId())
-                        .add("client_secret", this.appConfigGoogleAccount.getClientSecret())
-                        .add("redirect_uri", this.appConfigGoogleAccount.getRedirectUri())
-                        .add("code", code)
-                        .add("grant_type", GRAND_TYPE).build())
-                .execute()
-                .returnContent()
-                .asString();
-
-        JsonObject responseAsJson = new Gson().fromJson(response, JsonObject.class);
-        System.out.println("Get access token response:" + responseAsJson);
-        return new AccessTokenResponse(
-                responseAsJson.get("access_token").toString().replaceAll("\"", ""),
-                responseAsJson.get("expires_in").toString()
-        );
-    }
-
-    /**
-     * This method takes the access token provided by Google then use it to fetch
-     * the user's information from the authorized Google account
-     *
-     * @param accessToken
-     * @return
-     * @throws ClientProtocolException
-     * @throws IOException
-     */
-    private OpenIdClaims getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
-        final String API_END_POINT = "https://openidconnect.googleapis.com/v1/userinfo?scope=openid%20profile&access_token=";
-        String link = API_END_POINT + accessToken;
-        String response = Request.Get(link).execute().returnContent().asString();
-        System.out.println("response:" + response);
-        return new Gson().fromJson(response, OpenIdClaims.class);
-    }
 }
---
+++ src/main/java/com/example/javaee/interceptor/LoginInterceptor.java
@@ -0,0 +1,129 @@
+package com.example.javaee.interceptor;
+
+import com.example.javaee.beans.AppConfigGoogleAccount;
+import com.example.javaee.beans.SignInGoogleAccount;
+import com.example.javaee.dto.AccessTokenResponse;
+import com.example.javaee.dto.OpenIdClaims;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.IOException;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    private final SignInGoogleAccount signInGoogleAccount;
+
+    private final AppConfigGoogleAccount appConfigGoogleAccount;
+
+    public LoginInterceptor(
+            AppConfigGoogleAccount appConfigGoogleAccount,
+            SignInGoogleAccount signInGoogleAccount) {
+        this.appConfigGoogleAccount = appConfigGoogleAccount;
+        this.signInGoogleAccount = signInGoogleAccount;
+    }
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+        System.out.println("This message before execute the controller");
+
+        String code = request.getParameter("code");
+        System.out.println("code:" + code);
+        AccessTokenResponse accessTokenResponse = getToken(code);
+        System.out.println("accessToken:" + accessTokenResponse);
+        HttpSession session = request.getSession();
+        session.setAttribute("accessToken", accessTokenResponse.getAccessToken());
+        OpenIdClaims claims = getUserInfo(accessTokenResponse.getAccessToken());
+        System.out.println("claims:" + claims);
+        boolean comparision = claims.getEmail().equals(this.signInGoogleAccount.getEmail());
+        if (!comparision) {
+            final String CONTEXT_PATH = request.getContextPath();
+            RedirectView redirectView = new RedirectView(CONTEXT_PATH + "/index.htm");
+            redirectView.setContextRelative(true);
+            response.setStatus(HttpServletResponse.SC_FOUND);
+            response.setHeader("Location", redirectView.getUrl());
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView) throws Exception {
+        System.out.println("This message after controller and before execute the view");
+        // your code
+    }
+
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler, Exception ex) {
+        System.out.println("This message after execute the view");
+        // your code
+    }
+
+
+    /**
+     * This method use the code from the redirect request of Google to request the
+     * access token from Google for fetching User's Information
+     *
+     * @param code
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    private AccessTokenResponse getToken(String code) throws ClientProtocolException, IOException {
+        final String API_END_POINT = "https://oauth2.googleapis.com/token";
+        final String GRAND_TYPE = "authorization_code";
+
+        String response = Request.Post(API_END_POINT)
+                .bodyForm(Form.form()
+                        .add("client_id", this.appConfigGoogleAccount.getClientId())
+                        .add("client_secret", this.appConfigGoogleAccount.getClientSecret())
+                        .add("redirect_uri", this.appConfigGoogleAccount.getRedirectUri())
+                        .add("code", code)
+                        .add("grant_type", GRAND_TYPE).build())
+                .execute()
+                .returnContent()
+                .asString();
+
+        JsonObject responseAsJson = new Gson().fromJson(response, JsonObject.class);
+        System.out.println("Get access token response:" + responseAsJson);
+        return new AccessTokenResponse(
+                responseAsJson.get("access_token").toString().replaceAll("\"", ""),
+                responseAsJson.get("expires_in").toString()
+        );
+    }
+
+    /**
+     * This method takes the access token provided by Google then use it to fetch
+     * the user's information from the authorized Google account
+     *
+     * @param accessToken
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    private OpenIdClaims getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
+        final String API_END_POINT = "https://openidconnect.googleapis.com/v1/userinfo?scope=openid%20profile&access_token=";
+        String link = API_END_POINT + accessToken;
+        String response = Request.Get(link).execute().returnContent().asString();
+        System.out.println("response:" + response);
+        return new Gson().fromJson(response, OpenIdClaims.class);
+    }
+}
