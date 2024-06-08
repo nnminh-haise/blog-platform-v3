ec507f4892e0c92d69df1c3b6b3e8486e6ae5f14
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-06-08 14:49:10 +0700

    update: implemented creating category process

--- src/main/java/com/example/javaee/controller/CategoryController.java
+++ src/main/java/com/example/javaee/controller/CategoryController.java
@@ -1,15 +1,18 @@
 package com.example.javaee.controller;
 
+import com.example.javaee.dto.CreateCategoryDto;
 import com.example.javaee.dto.OpenIdClaims;
+import com.example.javaee.dto.UpdateCategoryDto;
+import com.example.javaee.helper.ServiceResponse;
+import com.example.javaee.model.Blog;
 import com.example.javaee.model.Category;
 import com.example.javaee.service.AdminService;
 import com.example.javaee.service.CategoryService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -57,13 +60,14 @@ public class CategoryController {
         }
         modelMap.addAttribute("adminInformation", claims.get());
 
-        modelMap.addAttribute("category", new Category());
+        modelMap.addAttribute("createCategoryDto", new CreateCategoryDto());
 
         return "category/insert";
     }
 
     @PostMapping("insert.htm")
     public String categoryInsertHandler(
+            @ModelAttribute("createCategoryDto") CreateCategoryDto createCategoryDto,
             HttpServletRequest request,
             ModelMap modelMap) {
         Optional<OpenIdClaims> claims = this.adminService.validateRequest(request);
@@ -74,8 +78,38 @@ public class CategoryController {
         }
         modelMap.addAttribute("adminInformation", claims.get());
 
-        modelMap.addAttribute("category", new Category());
+        ServiceResponse<Category> response = this.categoryService.create(createCategoryDto);
+        if (!response.isSuccess()) {
+            // TODO: add error handling here
+            return "redirect://index.hm";
+        }
 
-        return "category/insert";
+        return "redirect:/admin/categories/index.htm";
+    }
+
+    @GetMapping("/edit/{slug}.htm")
+    public String adminEditCategoryViewRenderer(
+            @PathVariable(name = "slug", required = true) String slug,
+            HttpServletRequest request,
+            ModelMap modelMap) {
+        Optional<OpenIdClaims> claims = this.adminService.validateRequest(request);
+        if (!claims.isPresent()) {
+            System.out.println(
+                    "[Admin Controller] (Blog Insert Route) ? Bad request - Cannot fetch access token claims -> Redirect back to landing page");
+            return "redirect:/index.htm";
+        }
+        modelMap.addAttribute("adminInformation", claims.get());
+
+        Optional<Category> requestedCategory = this.categoryService.findBySlug(slug);
+        if (!requestedCategory.isPresent()) {
+            // TODO: handle error here
+            return "redirect:/admin/index.htm";
+        }
+
+        UpdateCategoryDto updatingCategoryDto = new UpdateCategoryDto();
+        updatingCategoryDto.setName(requestedCategory.get().getName());
+        modelMap.addAttribute("updateCategoryDto", updatingCategoryDto);
+
+        return "admin/edit";
     }
 }
